f2322138cf5c7255a32b6d118e597b60de0fa4a3
bubbleee <euju718@gmail.com>
2018-10-29 15:40:26 +0900

    modified urls.py 2

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from blog.models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.order_by('created_date')
+    posts = Post.objects.all() #.order_by('created_date')
     #all() #filter(published_date__lte=timezone.now()).order_by('published_date') #filter(title__contains='title')
 #request 를 넘겨받아서, 템플릿을 보여준다. 매개변수 posts 이용.
     return render(request, 'blog/post_list.html', { 'posts': posts })
