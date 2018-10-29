ae484f8282cb0299de17f8654de766fccfbb520a
bubbleee <euju718@gmail.com>
2018-10-29 15:37:06 +0900

    modified urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-#from django.conf.urls import include, url
-#from django.contrib import admin
-from django.conf.urls import url
-from blog import views
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
-    #url(r'', include('blog.urls')),
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
